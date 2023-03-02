a9356d12078af4a1acd630acd3540a51bfb5e398
Daniel Palma <danivgy@gmail.com>
2023-03-02 20:40:25 +0000

    Test

--- talksheet/engine/engine.py
+++ talksheet/engine/engine.py
@@ -3,12 +3,11 @@ from langchain import OpenAI, SQLDatabaseChain, SQLDatabase
 from sqlalchemy import create_engine
 
 load_dotenv()
-
-engine = create_engine("duckdb:///:memory:")
-# create table
-engine.execute("CREATE TABLE src AS SELECT * FROM 'MOCK_DATA.csv';")
-db = SQLDatabase(engine=engine)
-llm = OpenAI(temperature=0)
-db_chain = SQLDatabaseChain(llm=llm, database=db, verbose=True)
-db_chain.run("What's the email address of Guillermo Poxton?")
-db_chain.run("How many males are there?")
+def read_data_from_csv(question: str, path_to_csv: str):
+    engine = create_engine("duckdb:///:memory:")
+    # create table
+    engine.execute(f"CREATE TABLE src AS SELECT * FROM '{path_to_csv}';")
+    db = SQLDatabase(engine=engine)
+    llm = OpenAI(temperature=0)
+    db_chain = SQLDatabaseChain(llm=llm, database=db, verbose=True)
+    return db_chain.run(question)
--- talksheet/talksheet.py
+++ talksheet/talksheet.py
@@ -1,60 +1,61 @@
+import pandas as pd
 from textual.app import App, ComposeResult
-from textual.containers import Container
 from textual.reactive import reactive
+from textual.widgets import (
+    DataTable,
+    Header,
+    Footer,
+    Input,
+    Static,
+    Button,
+    Markdown,
+    TextLog,
+)
+from textual.widget import Widget
 
-from textual.widgets import DataTable, Header, Footer, Input, Static, Button
-
-ROWS = [
-    ("lane", "swimmer", "country", "time"),
-    (4, "Joseph Schooling", "Singapore", 50.39),
-    (2, "Michael Phelps", "United States", 51.14),
-    (5, "Chad le Clos", "South Africa", 51.14),
-    (6, "László Cseh", "Hungary", 51.14),
-    (3, "Li Zhuhao", "China", 51.26),
-    (8, "Mehdy Metella", "France", 51.58),
-    (7, "Tom Shields", "United States", 51.73),
-    (1, "Aleksandr Sadovnikov", "Russia", 51.84),
-    (10, "Darren Burns", "Scotland", 51.84),
-]
-
-
-class TalkSheetChat(Static):
-    def compose(self) -> ComposeResult:
-        """Create child widgets of a stopwatch."""
-        yield Input(placeholder="Question...")
-        yield Button("Send", id="send", variant="success")
-
-
-class TalkSheetTable(Static):
-    data = reactive([])
-    data_table = DataTable()
-
-    def compose(self) -> ComposeResult:
-        yield Input(placeholder="sample_data/employees.csv", id="data_file")
-        yield self.data_table
-
-    def read_data_file(self, file_name):
-        with open(file_name) as f:
-            self.data = [line.split(",") for line in f.readlines()]
-        # TODO load into duckdb here and display in data_table
-
-    def on_input_submitted(self, event: Input.Submitted):
-        self.read_data_file(event.value)
-        rows = iter(ROWS)
-        self.data_table.add_columns(*next(rows))
-        self.data_table.add_rows(rows)
+from engine.engine import read_data_from_csv
 
 
 class TalkSheet(App):
     TITLE = "TalkSheet"
-    SUB_TITLE = "A Textual App"
-    CSS_PATH = "talksheet.css"
+    SUB_TITLE = "Talk to your Spreadsheets"
+    source_data_table = DataTable(id="source_table")
 
     def compose(self) -> ComposeResult:
         yield Header()
+        yield Input(placeholder="sample_data/employees.csv", id="data_file")
+        yield Input(placeholder="Question?", id="question")
+        yield self.source_data_table
+        yield TextLog()
         yield Footer()
-        yield Container(TalkSheetTable())
-        yield Container(TalkSheetChat())
+
+    def load_base_data(self, path_to_csv: str):
+        df = pd.read_csv(path_to_csv, nrows=5)
+        self.source_data_table.add_columns(*df.columns)
+        self.source_data_table.add_rows(df.values)
+
+    def on_ready(self) -> None:
+        """Called  when the DOM is ready."""
+        text_log = self.query_one(TextLog)
+        text_log.write("Welcome to TalkSheet!")
+
+    def on_input_submitted(self, event: Input.Submitted) -> None:
+        if event.input.id == "question":
+            self.ask_question()
+
+    def on_mount(self) -> None:
+        df = pd.read_csv("sample_data/users.csv", nrows=5)
+        self.source_data_table.add_columns(*df.columns)
+        self.source_data_table.add_rows(df.values)
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        self.ask_question()
+
+    def ask_question(self):
+        question = self.query_one("#question")
+        res = read_data_from_csv(question.value, "sample_data/users.csv")
+        text_log = self.query_one(TextLog)
+        text_log.write(res)
 
 
 app = TalkSheet()
