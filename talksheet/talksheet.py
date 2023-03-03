a026b57f252ad7bac60066f53c2b6286d4826219
Daniel Palma <danivgy@gmail.com>
2023-03-03 19:12:29 +0000

    Refactor

--- talksheet/talksheet.py
+++ talksheet/talksheet.py
@@ -1,14 +1,34 @@
+import asyncio
+import os
+
 import pandas as pd
+from dotenv import load_dotenv
+from langchain.agents import create_sql_agent
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+from langchain.llms.openai import OpenAI
+from langchain.sql_database import SQLDatabase
+from sqlalchemy import create_engine
 from textual.app import App, ComposeResult
 from textual.containers import Container
 from textual.widgets import (
     DataTable,
     Header,
-    Footer,
     Input,
 )
 
-from engine.engine import read_data_from_csv
+FORMAT_INSTRUCTIONS = """Use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer
+Final Answer: the final answer to the original input question, as a list of tuples and separate list of column names
+"""
+
+load_dotenv()
 
 
 class TalkSheet(App):
@@ -18,22 +38,25 @@ class TalkSheet(App):
     source_data_table = DataTable(id="source_table")
     results_data_table = DataTable(id="results_table")
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.engine = None
+        self.toolkit = None
+
     def compose(self) -> ComposeResult:
         yield Header()
         yield Container(
-            Input(
-                placeholder="path/to/file.csv", id="data_file", classes="question-box"
-            ),
+            Input(placeholder="path/to/file.csv", id="data_file"),
             self.source_data_table,
         )
         yield Container(
-            Input(placeholder="Question?", id="question"), self.results_data_table
+            Input(placeholder="Ask your Spreadsheet anything!", id="question"),
+            self.results_data_table,
         )
-        yield Footer()
 
-    def on_input_submitted(self, event: Input.Submitted) -> None:
+    async def on_input_submitted(self, event: Input.Submitted) -> None:
         if event.input.id == "question":
-            self.ask_question()
+            asyncio.create_task(self.ask_question())
         if event.input.id == "data_file":
             self.load_base_table(event.input.value)
 
@@ -42,22 +65,36 @@ class TalkSheet(App):
         self.source_data_table.clear(columns=True)
         self.source_data_table.add_columns(*df.columns)
         self.source_data_table.add_rows(df.values)
+        self.engine = create_engine("duckdb:///:memory:")
+        file_name = os.path.basename(path_to_csv).split(".")[0]
+        self.engine.execute(
+            f"CREATE TABLE {file_name} AS SELECT * FROM '{path_to_csv}';"
+        )
+        self.toolkit = SQLDatabaseToolkit(db=SQLDatabase(engine=self.engine))
 
     def on_mount(self) -> None:
         source_file_path = self.query_one("#data_file")
-        source_file_path.value = "sample_data/users.csv"
-        df = pd.read_csv(source_file_path.value, nrows=30)
-        self.source_data_table.add_columns(*df.columns)
-        self.source_data_table.add_rows(df.values)
+        source_file_path.value = "sample_data/purchase_items.csv"
+        self.load_base_table(source_file_path.value)
 
-    def ask_question(self):
+    async def ask_question(self):
         question = self.query_one("#question")
-        source_file_path = self.query_one("#data_file")
-        results = read_data_from_csv(question.value, source_file_path.value)
+        agent_executor = create_sql_agent(
+            llm=OpenAI(temperature=0),
+            toolkit=self.toolkit,
+            verbose=True,
+            format_instructions=FORMAT_INSTRUCTIONS,
+        )
+        results = agent_executor.run(question.value)
+        try:
+            parsed_results = eval(results)
+        except SyntaxError:
+            # Fall back to string
+            parsed_results = [[(results,)], ["result"]]
         self.results_data_table.clear(columns=True)
-        if len(results):
-            columns = results[1]
-            values = results[0]
+        if len(parsed_results):
+            columns = parsed_results[1]
+            values = parsed_results[0]
             self.results_data_table.add_columns(*columns)
             self.results_data_table.add_rows(values)
 
